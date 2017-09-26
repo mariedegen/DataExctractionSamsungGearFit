03b4fccf90f0c6a46cfd4dfe450b5bbfd25dc0ff
mariedegen <marie.degen@live.fr>
2017-09-26 11:09:27 +0200

    Creation of the watch project

---
+++ dataextraction/inc/dataextraction.h
@@ -0,0 +1,19 @@
+#ifndef __dataextraction_H__
+#define __dataextraction_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "dataextraction"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.dataextraction"
+#endif
+
+#endif /* __dataextraction_H__ */
