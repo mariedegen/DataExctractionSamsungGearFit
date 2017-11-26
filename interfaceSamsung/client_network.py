6910444dd941af81fa16b43d5c5ff007dcc1c4cf
passelacet <passelacet@users.noreply.github.com>
2017-11-26 19:24:52 +0100

    commit

--- interfaceSamsung/client_network.py
+++ interfaceSamsung/client_network.py
@@ -20,45 +20,47 @@ def clientNetwork(imageViewer):
     finish = True;
     end = 'END'
 
-    while finish:
-        print ("Searching ...")
-        try:
-
-          service_matches
-        except NameError:
-          service_matches = bluetooth.find_service(uuid = uuid)
-        else:
-          if not service_matches:
-                print ("without address")
-                service_matches = bluetooth.find_service(uuid = uuid)
-          else:
-                print ("with address")
-                service_matches_with_addr = bluetooth.find_service(uuid = uuid, address = host)
-                if service_matches_with_addr:
-                  service_matches = service_matches_with_addr
-                else:
-                  continue
-
-        if service_matches:
-          first_match = service_matches[0]
-          port = first_match["port"]
-          name = first_match["name"]
-          host = first_match["host"]
-          print ("Connecting to ", name, " on ", host)
-          sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-          sock.connect((host, port))
-
-          data = sock.recv(8);
-          while True: 
-              data = sock.recv(8)
-              if data:
-                tabData.append(data.decode('latin1'))
-              if tabData.endswith(end):
-                break
-              elif not data :
-                break
-        print("".join(tabData))
-    sock.close()
+    tabData = [0.0,75.0,73.0,76.0,78.0,0.0]
+    time.sleep(5)
+##    while finish:
+##        print ("Searching ...")
+##        try:
+##
+##          service_matches
+##        except NameError:
+##          service_matches = bluetooth.find_service(uuid = uuid)
+##        else:
+##          if not service_matches:
+##                print ("without address")
+##                service_matches = bluetooth.find_service(uuid = uuid)
+##          else:
+##                print ("with address")
+##                service_matches_with_addr = bluetooth.find_service(uuid = uuid, address = host)
+##                if service_matches_with_addr:
+##                  service_matches = service_matches_with_addr
+##                else:
+##                  continue
+##
+##        if service_matches:
+##          first_match = service_matches[0]
+##          port = first_match["port"]
+##          name = first_match["name"]
+##          host = first_match["host"]
+##          print ("Connecting to ", name, " on ", host)
+##          sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+##          sock.connect((host, port))
+##
+##          data = sock.recv(8);
+##          while True: 
+##              data = sock.recv(8)
+##              if data:
+##                tabData.append(data.decode('latin1'))
+##              if tabData.endswith(end):
+##                break
+##              elif not data :
+##                break
+##        print("".join(tabData))
+##    sock.close()
 
     imageViewer.data = tabData
 
