f8ebb5a9dc6fa578132a20720a2c2695cb1efaec
passlacet <pauline.lauron@orange.fr>
2018-01-11 12:14:09 +0100

    suppression de fichiers inutiles + ajout des données

--- interfaceSamsung/client_network.py
+++ interfaceSamsung/client_network.py
@@ -18,59 +18,62 @@ def clientNetwork(imageViewerSamsung):
     finish = True
     end = 'END'
 
-    tabData = ["0","1","9","3","0"]
-    time.sleep(2)
+    """tabData = ["0","1","9","3","0"]
+    time.sleep(2)"""
     
-##    while finish:
-##        print ("Searching ...")
-##        try:
-##
-##          service_matches
-##        except NameError:
-##          service_matches = bluetooth.find_service(uuid = uuid)
-##        else:
-##          if not service_matches:
-##                print ("without address")
-##                service_matches = bluetooth.find_service(uuid = uuid)
-##          else:
-##                print ("with address")
-##                service_matches_with_addr = bluetooth.find_service(uuid = uuid, address = host)
-##                if service_matches_with_addr:
-##                  service_matches = service_matches_with_addr
-##                else:
-##                  continue
-##
-##        if service_matches:
-##          first_match = service_matches[0]
-##          port = first_match["port"]
-##          name = first_match["name"]
-##          host = first_match["host"]
-##          print ("Connecting to ", name, " on ", host)
-##          sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-##          try:
-##            sock.connect((host, port))
-##          
-##            data = sock.recv(9);
-##            while True: 
-##                data = sock.recv(8)
-##                if data:
-##                  dataStr += data.decode('latin1')
-##                if dataStr.endswith(end):
-##                  break
-##                elif not data :
-##                  break
-##            finish = False
-##          except:
-##              e = sys.exc_info()[0]
-##              print("error:", e)
-##    print("data:", dataStr)
-##    tabData = conversionChartoFloat(dataStr)
-##    sock.close()
-##    del service_matches
-##    dataStr = ""
+    while finish:
+        print ("Searching ...")
+        try:
+
+          service_matches
+        except NameError:
+          service_matches = bluetooth.find_service(uuid = uuid)
+        else:
+          if not service_matches:
+                print ("without address")
+                service_matches = bluetooth.find_service(uuid = uuid)
+          else:
+                print ("with address")
+                service_matches_with_addr = bluetooth.find_service(uuid = uuid, address = host)
+                if service_matches_with_addr:
+                  service_matches = service_matches_with_addr
+                else:
+                  continue
+
+        if service_matches:
+          first_match = service_matches[0]
+          port = first_match["port"]
+          name = first_match["name"]
+          host = first_match["host"]
+          print ("Connecting to ", name, " on ", host)
+          sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+          try:
+            sock.connect((host, port))
+          
+            data = sock.recv(9);
+            while True: 
+                data = sock.recv(8)
+                if data:
+                  dataStr += data.decode('latin1')
+                if dataStr.endswith(end):
+                  break
+                elif not data :
+                  break
+            finish = False
+          except:
+              e = sys.exc_info()[0]
+              print("error:", e)
+    print("data:", dataStr)
+    tabData = conversionChartoFloat(dataStr)
+    sock.close()
+    del service_matches
+    dataStr = ""
 
     imageViewerSamsung.data = tabData
     imageViewerSamsung.actualizeGraphic()
+
+    while(1):
+        time.sleep(2)
 
 def conversionChartoFloat(data): 
     tabData = []
